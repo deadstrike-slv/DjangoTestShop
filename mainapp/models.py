7d36d7a870aef96b64fae99118aa36386e74009e
deadstrike-slv <deadstrike.slv@gmail.com>
2021-06-16 23:42:39 +0300

    Created specification templates, urls Fix: rename 'rma' to 'ram' in models QoA: region comments

--- mainapp/models.py
+++ mainapp/models.py
@@ -7,10 +7,17 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
+from django.urls import reverse
 
 User = get_user_model()
 
 
+def get_product_url(obj, viewname):
+    ct_model = obj.__class__.__meta.model_name
+    return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
+
+
+# region custom Exceptions
 class MinResolutionError(Exception):
     pass
 
@@ -18,6 +25,8 @@ class MinResolutionError(Exception):
 class MaxResolutionError(Exception):
     pass
 
+
+# endregion
 
 class LatestProductsManager:
 
@@ -92,16 +101,20 @@ class Product(models.Model):
         super().save(*args, **kwargs)
 
 
+# region Products inheritance
 class Notebook(Product):
     diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
     display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
     proc_freq = models.CharField(max_length=255, verbose_name='Частота процессора')
-    rma = models.CharField(max_length=255, verbose_name='Оперативная память')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
     video = models.CharField(max_length=255, verbose_name='Графический чип')
     time_without_charge = models.CharField(max_length=255, verbose_name='Время работы аккумулятора')
 
     def __str__(self):
         return f'{self.category.name} : {self.title}'
+
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
 
 
 class Smartphone(Product):
@@ -109,7 +122,7 @@ class Smartphone(Product):
     display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
     resolution = models.CharField(max_length=255, verbose_name='Разрешение экрана')
     accum_volume = models.CharField(max_length=255, verbose_name='Объем аккумулятора')
-    rma = models.CharField(max_length=255, verbose_name='Оперативная память')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
     sd = models.BooleanField(default=True)
     sd_volume_max = models.CharField(max_length=255, verbose_name='Максимальный обьем SD')
     main_cam_mp = models.CharField(max_length=255, verbose_name='Главная камера')
@@ -118,6 +131,11 @@ class Smartphone(Product):
     def __str__(self):
         return f'{self.category.name} : {self.title}'
 
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
+
+
+# endregion
 
 class CartProduct(models.Model):
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
